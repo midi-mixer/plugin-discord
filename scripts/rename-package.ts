479018b75114148f852a5f789aa310010bcbbf4d
Jack Williams <jpwilliamsphotography@gmail.com>
2021-06-06 20:42:43 +0100

    chore: get name and version from npm'd package

--- scripts/rename-package.ts
+++ scripts/rename-package.ts
@@ -1,5 +1,5 @@
 import { renameSync } from "fs";
-import { name, version } from "../plugin.json";
+import { name, version } from "../package.json";
 
 const baseName = [name, version].join("-");
 const expectedName = `${baseName}.tgz`;
