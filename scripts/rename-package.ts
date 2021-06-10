3b6ad21c27357c3892adc46f31bb5558dab31621
Jack Williams <jpwilliamsphotography@gmail.com>
2021-06-10 22:27:57 +0100

    chore(build): use midi-mixer-cli to pack plugin to file

--- scripts/rename-package.ts
+++
@@ -1,8 +0,0 @@
-import { renameSync } from "fs";
-import { name, version } from "../package.json";
-
-const baseName = [name, version].join("-");
-const expectedName = `${baseName}.tgz`;
-const targetName = `${baseName}.midiMixerPlugin`;
-
-renameSync(expectedName, targetName);
